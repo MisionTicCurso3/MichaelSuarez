014dd4941f74f6baff91c037f89959e9850ee948
MYSP1079 <miyamidsuarezp@gmail.com>
2021-08-12 21:11:01 -0500

    Ejercicio Heladeria

---
+++ Codigo/Ejercicio2/Helados.py
@@ -0,0 +1,60 @@
+print ( "Buen Dia, como deseas tu helado" )
+print ( "Di si o no respecto a cada pregunta" )
+
+heladosolo = 1900
+
+print ( "¿Quieres tu helado con oreo?" )
+Oreo = input ()
+
+HeladoSolo = 1900
+
+if ( Oreo == "si" ):
+    Resultado = 2900
+    (1900 + 1000 == Resultado)
+    print ( "Se agrego 1000 mas total", Resultado )
+else:
+    Resultado1 = 1900 + 0
+    (1900 + 0 == Resultado1)
+    print ( "Seguimos total", Resultado1 )
+
+
+print ( "¿Quieres tu helado con Kitkat?" )
+Kitkat = input ()
+
+if ( Kitkat == "si" ):
+    Resultado2 = Resultado + 1500
+    (1500 + Resultado == Resultado2)
+    print ( "Se agrego 1.500 mas total", Resultado2 )
+
+else:
+    Resultado3 = Resultado1 + 0
+    (0 + Resultado1 == Resultado3)
+    print ( "Seguimos total", Resultado3 )
+
+
+print ( "¿Quieres tu helado con Brownie?")
+Brownie = input ()
+
+if ( Brownie == "si" ):
+    Resultado4 = Resultado2 + 2500
+    (2500 + Resultado2 == Resultado4)
+    print ( "Se agrego 2500 mas total", Resultado4 )
+
+else:
+    Resultado5 = Resultado3 + 0
+    (0 + Resultado3 == Resultado5)
+    print ( "Seguimos total", Resultado5 )
+
+
+print ( "¿Quieres tu helado con Barquillo?" )
+Barquillo = input ()
+
+if (Barquillo == "si" ):
+    Resultado6 = Resultado4 + 900
+    (900 + Resultado4 == Resultado6)
+    print ( "Se agrego 900 mas total", Resultado6)
+
+else:
+    Resultado7 == Resultado5 + 0
+    (0 + Resultado5 == Resultado7)
+    print ( "Terminamos total", Resultado7)
